7b9aabc14a0be060a7a807eef18711181cc74ab5
André Motz <me@andremotz.com>
2022-04-18 17:23:39 +0200

    Update 1_start.sh

--- 1_start.sh
+++ 1_start.sh
@@ -5,11 +5,5 @@ source sync_config.cfg
 $logger "1_start.sh Starting SyncAllScript"
 BOOTUP_MAIL_CONTENT=$(cat $MAIL_CONTENT)
 
-sendemail -t $MAIL_RECEIVER -u "Homeserver online" $MAIL_OPTIONS -m $BOOTUP_MAIL_CONTENT 
-
-smartctl -t short /dev/sda
-smartctl -t short /dev/sdb
-smartctl -t short /dev/sdc
-smartctl -t short /dev/sdd
-smartctl -t short /dev/sde
+sendemail -t $MAIL_RECEIVER -u "Homeserver online" $MAIL_OPTIONS -m $BOOTUP_MAIL_CONTENT
 
